5008cebf5c4109a3af93b13133c06511b1e36dd7
Andrew Tam <andrew.tam@cra-arc.gc.ca>
2023-09-16 01:37:39 -0400

    Update alpha-banner.js

--- js/alpha-banner.js
+++ js/alpha-banner.js
@@ -1,4 +1,4 @@
 // if you want the banner off, just comment out the file
 
 var alphaBanner = document.getElementsByTagName('BODY')[0];
-alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">This webpage is in alpha</h2><div class="container well"><small><span class="alpha-label"><strong>Alpha</strong></span>&nbsp;&nbsp; This is an experimental version for the Canada Revenue Agency.</small></div></section >');
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top well" role="banner"><h2 class="wb-inv">This webpage is in alpha</h2><div class="container"><small><span class="alpha-label"><strong>Alpha</strong></span>&nbsp;&nbsp; This is an experimental version for the Canada Revenue Agency.</small></div></section >');
