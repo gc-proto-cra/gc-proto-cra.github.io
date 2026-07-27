c75012020293c72c3320e396c99d80f30b92299a
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2026-07-27 16:27:24 -0400

    update task scripts

---
+++ fable-tasks/chatbot/scripts/task-ver-show-hide-elm.js
@@ -0,0 +1,20 @@
+// 1. Get the 'ver' parameter from the URL query string
+const urlParams = new URLSearchParams(window.location.search), 
+    currentVer = parseInt(urlParams.get("ver"), 10), 
+
+    // 2. Select all elements with the 'data-ver' attribute
+    verElements = document.querySelectorAll("[data-ver]");
+
+if (!isNaN(currentVer)) {
+    verElements.forEach(element => {
+        const allowedVersions = JSON.parse(element.getAttribute("data-ver"));
+
+        if (allowedVersions.includes(currentVer) === true) {
+            // Show this element by removing the hide class
+            element.classList.remove("wb-inv");
+        } else {
+            // Hide the other elements by adding the hide class
+            element.classList.add("wb-inv");
+        }
+    });
+}
