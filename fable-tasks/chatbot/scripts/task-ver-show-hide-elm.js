872d5e94af560bcb6cf34c3adf320f0b9512699f
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2026-07-29 14:07:14 -0400

    update version select script and references

--- fable-tasks/chatbot/scripts/task-ver-show-hide-elm.js
+++ fable-tasks/chatbot/scripts/task-ver-show-hide-elm.js
@@ -1,5 +1,7 @@
 // 1. Get the 'ver' parameter from the URL query string
-const showHideURLParams = new URLSearchParams(globalThis.location.search), 
+const choiceMin = 1, 
+    choiceMax = 4,
+    showHideURLParams = new URLSearchParams(globalThis.location.search), 
     currentVer = parseInt(showHideURLParams.get("ver"), 10), 
 
     // 2. Select all elements with the 'data-ver' attribute
@@ -9,7 +11,7 @@ if (!isNaN(currentVer)) {
     verElements.forEach(element => {
         const allowedVersions = JSON.parse(element.getAttribute("data-ver"));
 
-        if (allowedVersions.includes(currentVer) === true) {
+        if (allowedVersions.includes(currentVer) === true || currentVer >= choiceMin || currentVer <= choiceMax) {
             // Show this element by removing the hide class
             element.classList.remove("wb-inv");
         } else {
